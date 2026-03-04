5a206b384f65ed7d58f3879ca4fd1d4af0f69d8f
Guilhermina Moreira D'Onofrio <106886446+guismdonofrio@users.noreply.github.com>
2026-03-04 19:53:12 -0300

    Atualização.
    Feito novamente pelo VScode

---
+++ Atividades/src/Atv1_Media.java
@@ -0,0 +1,16 @@
+public class Atv1_Media {
+
+   public static void atv2media (String []args) {
+       
+      double janeiro = 15000;
+      double fevereiro = 23000;
+      double marco = 17000;
+      
+      double total = janeiro + fevereiro + marco;
+      double media = total/3;
+      
+      System.out.println("Total por trimentre: " + total + "\nMedia mensal: " + media );
+      //System.out.println();
+      
+   }
+}
---
+++ Atividades/src/Atv_ex.java
@@ -0,0 +1,10 @@
+public class Atv_ex {
+    public static void atv1(String[] args) {
+
+        System.out.println("Primeira linha");
+        System.out.println("Segunda linha");
+
+        //System.out.println("Primeira linha\nSegunda linha");
+
+    }
+}
