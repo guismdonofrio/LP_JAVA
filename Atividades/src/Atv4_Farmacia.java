1879ccda883c1d83c02e23604e8ddc8c1411a5ba
Guilhermina Moreira D'Onofrio <106886446+guismdonofrio@users.noreply.github.com>
2026-03-16 20:30:01 -0300

    adicionado o comentário de exemplo de main de farmácia e construtor

--- Atividades/src/Atv4_Farmacia.java
+++ Atividades/src/Atv4_Farmacia.java
@@ -16,6 +16,13 @@ public class Atv4_Farmacia {
     double preco;
     String validade;
     
+    public Atv4_Farmacia(String nome, String fabricante, double preco, String validade) {
+        this.nome = nome;
+        this.fabricante = fabricante;
+        this.preco = preco;
+        this.validade = validade;
+    }
+    
     void vender() {
         System.out.println("Medicamento vendido.");
     }
@@ -24,4 +31,22 @@ public class Atv4_Farmacia {
         System.out.println("Medicamento aplicado.");
     }
 }
+//Main
+/*
+public class Main {
 
+    public static void main(String[] args) {
+
+        Atv4_Farmacia medicamento = new Atv4_Farmacia(
+            "Paracetamol",
+            "EMS",
+            12.50,
+            "12/2026"
+        );
+
+        medicamento.vender();
+        medicamento.aplicar();
+    }
+
+}
+ */
